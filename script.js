06f4a8d292dfeb72bb19dbf49e3cca77c125b7d3
Anastasiia Chavdar <chavdaranastasiya85@yandex.ru>
2025-07-21 11:42:42 +0300

    lesson-03

---
+++ script.js
@@ -0,0 +1,7 @@
+let nameUser = prompt('Escribe su nombre');
+let patronymUser = prompt('Escribe su nombre patronímico');
+let surnameUser = prompt('Escribe su apellido');
+
+alert(`Hola, me llamo ${surnameUser} ${nameUser} ${patronymUser}`);
+
+console.log(`Hola, me llamo ${surnameUser} ${nameUser} ${patronymUser}`);
