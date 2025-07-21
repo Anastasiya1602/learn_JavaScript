64f7b8043bd0d3be38ce6eccbca6f1350c65c555
Anastasiia Chavdar <chavdaranastasiya85@yandex.ru>
2025-07-21 11:55:49 +0300

    lesson-03

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let dataUser = confirm ('Хотите ввести свои ФИО? Если нет, Вам все равно придётся это сделать, я могла бы написать здесь условие, но ДЗ это не подразумевало)))');
+alert(dataUser);
 
 let nameUser = prompt('Escribe su nombre', '');
 let patronymUser = prompt('Escribe su nombre patronímico', '');
