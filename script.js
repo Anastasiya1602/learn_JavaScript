c866654b2f415acd92151237edcb903a1d666407
Anastasiia Chavdar <chavdaranastasiya85@yandex.ru>
2025-08-04 19:03:40 +0300

    js, numbers

---
+++ script.js
@@ -0,0 +1,12 @@
+let numOne = +prompt('введите первое число', '');
+let numTwo = +prompt('введите второе число', '');
+
+alert(numOne+numTwo);
+alert(numOne-numTwo);
+alert(numOne*numTwo);
+alert(numOne/numTwo);
+
+console.log(numOne+numTwo);
+console.log(numOne-numTwo);
+console.log(numOne*numTwo);
+console.log(numOne/numTwo);
